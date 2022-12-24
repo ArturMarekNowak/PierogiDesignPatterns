496af361c951004c8ef752c5da923570ba54027e
Artur Nowak <arturmareknowal@gmail.com>
2022-12-24 20:07:40 +0100

    Add Pierogi-Singleton project

---
+++ Creational.Singleton/Model/Pierogi.cs
@@ -0,0 +1,20 @@
+﻿namespace Creational.Singleton.Model;
+
+public sealed class Pierogi
+{
+    public Guid Guid { get; }
+    public int Count { get; }
+    public string Type { get; }
+    
+    public Pierogi(Guid guid, int count, string type)
+    {
+        Guid = guid;
+        Count = count;
+        Type = type;
+    }
+
+    public override string ToString()
+    {
+        return $"Guid: {Guid}, Count: {Count}, Type: {Type}";
+    }
+}
---
+++ Creational.Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using Creational.Singleton.DesignPatters;
+
+namespace Creational.Singleton;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine($"Is pierogi singleton instantiated: {PierogiSingleton.IsInstanceCreated}");
+        
+        var firstPortionOfPierogi = PierogiSingleton.Instance.ServePierogiWithBlueberries();
+        Console.WriteLine($"{firstPortionOfPierogi}, Yummy :D");
+        
+        Console.WriteLine($"Is pierogi singleton instantiated: {PierogiSingleton.IsInstanceCreated}");
+        
+        var secondPortionOfPierogi = PierogiSingleton.Instance.ServePierogiWithPotatoesAndCheeseButAlsoWithYoghurtAndKetchup();
+        Console.WriteLine($"{secondPortionOfPierogi}, aaaah what is this sacrilege? :O");
+        
+        Console.WriteLine($"Plates of pierogi's served: {PierogiSingleton.Instance.PlatesServed}");
+    }
+}
