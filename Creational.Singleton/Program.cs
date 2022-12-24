5f2507df6282960a6b440a8df295c21b935a67e7
Artur Nowak <arturmareknowal@gmail.com>
2022-12-24 20:40:11 +0100

    Change execution of Pierogi Singleton Program.cs to concurrent one

--- Creational.Singleton/Program.cs
+++ Creational.Singleton/Program.cs
@@ -4,18 +4,26 @@ namespace Creational.Singleton;
 
 class Program
 {
-    public static void Main()
+    public static async Task Work(int delay)
     {
+        await Task.Delay(delay);
         Console.WriteLine($"Is pierogi singleton instantiated: {PierogiSingleton.IsInstanceCreated}");
-        
+        await Task.Delay(delay);
         var firstPortionOfPierogi = PierogiSingleton.Instance.ServePierogiWithBlueberries();
         Console.WriteLine($"{firstPortionOfPierogi}, Yummy :D");
-        
         Console.WriteLine($"Is pierogi singleton instantiated: {PierogiSingleton.IsInstanceCreated}");
-        
+        await Task.Delay(delay);
         var secondPortionOfPierogi = PierogiSingleton.Instance.ServePierogiWithPotatoesAndCheeseButAlsoWithYoghurtAndKetchup();
         Console.WriteLine($"{secondPortionOfPierogi}, aaaah what is this sacrilege? :O");
-        
-        Console.WriteLine($"Plates of pierogi's served: {PierogiSingleton.Instance.PlatesServed}");
+        await Task.Delay(delay);
+    }
+    
+    public static async Task Main()
+    {
+        Task.WaitAll(new Task[]
+        {
+            Task.Run(() => Work(500)),
+            Task.Run(() => Work(2500))
+        });
     }
 }
