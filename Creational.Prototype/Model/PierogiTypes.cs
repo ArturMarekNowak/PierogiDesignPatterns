97a979ea234fbb8f916eb50d79ea3519730a9cdf
Artur Nowak <arturmareknowal@gmail.com>
2022-12-24 22:08:34 +0100

    Add Pierogi-Prototype project

---
+++ Creational.Prototype/Model/PierogiTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace Creational.Prototype.Model;
+
+public static class PierogiTypes
+{
+    public const string WithPotatoesAndCheese = "with potatoes and cheese";
+    public const string WithGroat = "with groat";
+    public const string WithCabbage = "with cabbage";
+    public const string WithMeat = "with meat";
+    public const string WithStrawberries = "with strawberries";
+    public const string WithBlueberries = "with blueberries";
+    public const string WithPotatoesAndCheeseButAlsoWithYoghurtAndKetchup = "with potatoes and cheese but also with yoghurt and ketchup";
+}
---
+++ Creational.Prototype/Program.cs
@@ -0,0 +1,18 @@
+﻿using Creational.Prototype.DesignPatters;
+
+namespace Creational.Prototype;
+
+class Program
+{
+    public static void Main()
+    {
+        var firstPortion = new PierogiWithBlueberries(10);
+        Console.WriteLine(firstPortion);
+        var secondPortion = firstPortion.Clone();
+        Console.WriteLine(secondPortion);
+        firstPortion.Count = 5;
+        Console.WriteLine(firstPortion);
+        Console.WriteLine(secondPortion);
+
+    }
+}
