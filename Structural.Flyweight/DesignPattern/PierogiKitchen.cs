81d35c5daa1274e0207059c34023505dbc945d99
Artur Nowak <arturmareknowal@gmail.com>
2022-12-31 00:09:52 +0100

    Add Flyweight project

---
+++ Structural.Flyweight/DesignPattern/PierogiKitchen.cs
@@ -0,0 +1,17 @@
+﻿namespace Structural.Flyweight.DesignPattern;
+
+public class PierogiKitchen
+{
+    private Dictionary<string, Pierogi> _pierogis { get; } = new();
+
+    public PierogiKitchen()
+    {
+        _pierogis.Add("a", new PierogiWithCabbage(10));
+        _pierogis.Add("b", new PierogiWithMeat(10));
+        _pierogis.Add("c", new PierogiWithMeat(10));
+    }
+    public Pierogi GetPierogiPortion(string key)
+    {
+        return _pierogis[key];
+    }
+}
---
+++ Structural.Flyweight/Program.cs
@@ -0,0 +1,23 @@
+﻿using Structural.Flyweight.DesignPattern;
+
+namespace Structural.Flyweight;
+
+sealed class Program
+{
+    public static void Main()
+    {
+        var pierogiKitchen = new PierogiKitchen();
+        
+        var firstPortion = pierogiKitchen.GetPierogiPortion("a");
+        firstPortion.AskForSeconds(5);
+        Console.WriteLine(firstPortion);
+        
+        var secondPortion = pierogiKitchen.GetPierogiPortion("b");
+        secondPortion.AskForSeconds(10);
+        Console.WriteLine(secondPortion);
+        
+        var thirdPortion = pierogiKitchen.GetPierogiPortion("c");
+        thirdPortion.AskForSeconds(15);
+        Console.WriteLine(thirdPortion);
+    }
+}
