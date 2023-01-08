22d918169a86bfb34220e5cee136a3175726cfc6
Artur Nowak <arturmareknowal@gmail.com>
2023-01-08 18:28:07 +0100

    Add Bridge project

---
+++ Structural.Bridge/Interfaces/IPierogi.cs
@@ -0,0 +1,12 @@
+﻿using Structural.Bridge.Model;
+
+namespace Structural.Bridge.Interfaces;
+
+public interface IPierogi
+{
+    public Guid Guid { get; }
+    public int Count { set; get; }
+    public PierogiType Type { get; set; }
+
+    public string ToString();
+}
