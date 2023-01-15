568ae1563987987a531a238ef3ee1f4545fb3166
Artur Nowak <arturmareknowal@gmail.com>
2023-01-15 15:10:38 +0100

    Add Observer project

---
+++ Behavioral.Observer/DesignPattern/IPierogiObserver.cs
@@ -0,0 +1,8 @@
+﻿namespace Behavioral.Observer.DesignPattern;
+
+public interface IPierogiObserver
+{
+    public string Name { get; init; }
+
+    public void Update(IPierogiWarehouse pierogiWarehouse);
+}
---
+++ Behavioral.Observer/DesignPattern/IPierogiWarehouse.cs
@@ -0,0 +1,10 @@
+﻿namespace Behavioral.Observer.DesignPattern;
+
+public interface IPierogiWarehouse
+{
+    public int NumberOfPierogi { get; set; }
+    
+    public void Attach(IPierogiObserver pierogiObserver);
+    public void Detach(IPierogiObserver pierogiObserver);
+    public void Notify();
+}
