9541d696abf9d54edcb34154c07e5851cf434928
Artur Nowak <arturmareknowal@gmail.com>
2023-01-15 15:53:56 +0100

    Add State project

---
+++ Behavioral.State/Program.cs
@@ -0,0 +1,13 @@
+﻿using Behavioral.State.DesignPattern;
+
+public class Program
+{
+    public static void Main()
+    {
+        var pierogiKitchen = new PierogiKitchen(new Undercooked());
+        
+        pierogiKitchen.Request();
+        pierogiKitchen.Request();
+        pierogiKitchen.Request();
+    }
+}
